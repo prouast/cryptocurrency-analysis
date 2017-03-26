6ca9d1b2d8e5ad7c30280ceb4eebd764e5938390
Philipp Rouast <prouast@gmail.com>
2017-03-26 23:16:33 +1100

    Wrote function to generate dataframe with complete daily information for a set of currencies

--- analysis.R
+++ analysis.R
@@ -8,5 +8,16 @@ vals$id <- NULL # Drop database IDs
 rm(con) # Close database connection
 vals$datetime <- as.Date(vals$datetime)
 
-### Plots with bitcoin
-plot(vals[which(vals$currency_slug=='bitcoin'),]$price_usd, type="l")
+### Analysis
+# Generates a dataframe with complete daily information for a set of currencies
+analysis.data <- function(currencies, data) {
+  temp <- lapply(currencies, FUN=function(x) subset(data, currency_slug==x))
+  temp <- Reduce(function(df1, df2) merge(df1, df2, by="datetime"), temp)
+  colnames(temp) <- c("datetime", sapply(currencies, function(slug) sapply(colnames(vals)[c(1:5,7)], function(x) paste(x, slug, sep="_"))))
+  data.frame(temp)
+}
+# Example
+temp <- analysis.data(c("bitcoin","ethereum","dash","pivx"), vals)
+
+
+
