f9f7e92ad3a51718f118497b07a037b69324c406
Philipp Rouast <prouast@gmail.com>
2017-03-26 16:10:12 +1100

    First plot

--- analysis.R
+++ analysis.R
@@ -1,7 +1,12 @@
-# Import sqlite database
-
+### Import data from sqlite
 # install.packages("RSQLite")
 library(DBI)
 con <- dbConnect(RSQLite::SQLite(), dbname='database.db')
 currencies <- dbGetQuery(con, "SELECT * FROM currency")
 vals <- dbGetQuery(con, "SELECT * FROM vals")
+vals$id <- NULL # Drop database IDs
+rm(con) # Close database connection
+vals$datetime <- as.Date(vals$datetime)
+
+### Plots with bitcoin
+plot(vals[which(vals$currency_slug=='bitcoin'),]$price_usd, type="l")
