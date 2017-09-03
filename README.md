11afa23e0eb7a242008619d3ed7eb1e85cf8bd83
Philipp Rouast <prouast@gmail.com>
2017-09-03 19:06:09 +1000

    Started reorganising, added volatility analysis and updated with recent data

--- README.md
+++ README.md
@@ -5,59 +5,74 @@
 Having followed the cryptocurrency market for a while now, I decided to do some exploring in the data available from [coinmarketcap](https://coinmarketcap.com).
 Especially in light of the increasing number of successful coins and [decreasing Bitcoin dominance in terms of market capitalisation](https://coinmarketcap.com/charts/#btc-percentage), I assume many investors are eager to understand the dynamics in this market.
 
+To replicate, first head over to [coinmarketcap-scraper](https://github.com/prouast/coinmarketcap-scraper);
+this lets you download data from [coinmarketcap](https://coinmarketcap.com) into a local database.
+The script `analysis.R` can then be run on this database.
+
 ## Progress
 
-*Data updated 31/08/2017. Restricted some visualizations to data from 2017*
+*Data updated 03/09/2017. Some improvements, more to come.*
 
 This is a work in progress. Steps taken so far:
 
-1. Obtain and clean data from [coinmarketcap](https://coinmarketcap.com) using [coinmarketcap-scraper](https://github.com/prouast/coinmarketcap-scraper). This gives us daily closing prices for each coin.
+1. Obtain and clean daily closing prices for each coin. This includes interpolation for missing data.
 
-2. Calculate **Overall market capitalisation** and **Herfindahl index** to illustrate the change in market capitalisation and competition between coins.
+2. Use daily closing prices to calculate daily overall market statistics. For the sake of these calculations, the overall market is interpreted as an index/portfolio.
 
-	> The Herfindahl index measures competition in a market by summing the squares of all competitor market shares. It ranges from 1/N (highly competitive) to 1 (high concentration), where N is the number of competitors. [More information.](https://en.wikipedia.org/wiki/Herfindahl_index)
+  - **Overall market capitalisation**: The sum of all coin valuations, i.e., closing prices multiplied with circulating supply as given by coinmarketcap. *Please note that this is a controversial metric.*
+  - **Overall market return**: We use logarithmic returns to make daily changes in overall market value easily comparable. [More information.](https://en.wikipedia.org/wiki/Rate_of_return#Logarithmic_or_continuously_compounded_return)
+  - **Overall market volatility**: The annualized overall market volatility illustrates the degree in variation of the changes in overall market capitalisation. For each day, the value is based on the logarithmic overall market returns of the last 30 days. [More information.](https://en.wikipedia.org/wiki/Volatility_(finance))
+  - **Herfindahl index**: To illustrate the change in competition between coins. The Herfindahl index measures competition in a market by summing the squares of all competitor market shares. It ranges from 1/N (highly competitive) to 1 (high concentration), where N is the number of competitors. [More information.](https://en.wikipedia.org/wiki/Herfindahl_index)
 
-	- Here, we see that both market capitalisation and competition are increasing over time. Since the recent correction, the decrease in concentration has stagnated.
+  > Here, we can observe how the market capitalisation has increased in 2017 by a factor of up to 20x.
+  > Up until the recent correction, this development was mirrored by an increase in competition. More recently, this development has stagnated.
+  > In the data on returns and volatility we find volatility clustering, which is common in financial time series: Times of relative calm, and times of sudden price movements.
+  > Looking at the annualized volatility series, the clusters of high volatility have generally been decreasing in magnitude. The hype of 2017 has caused slightly higher volatility again, however less than we observe for some periods between 2013 and 2015.
+  > Note that the annualized volatilities of stock indices such as the S&P 500 are typically much lower, compared to the early crypto days up to an **order of magnitude**.
 
-	![Market cap and Herfindahl index](https://raw.githubusercontent.com/prouast/cryptocurrency-analysis/master/Market-cap-herfindahl.png?style=centerme)
+	![Market statistics](https://raw.githubusercontent.com/prouast/cryptocurrency-analysis/master/Market-statistics.png?style=centerme)
 
-3. Calculate the **daily return** for each coin.
+3. Calculate statistics for each individual coin.
 
-	> The rate of return is the income from an investment expressed as a proportion of the original investment. Hence, here it is `(price(t) - price(t-1)) / price(t-1)`. [More information.](https://en.wikipedia.org/wiki/Rate_of_return)
+  - **Market capitalisation**: A given coin's valuation, i.e., closing price multiplied with circulating supply as given by coinmarketcap. *Please note that this is a controversial metric.*
+  - **Return**: The daily logarithmic return of a given coin.
+  - **Volatility**: The annualized volatility of a given coin; based on the logarithmic return from the last 30 days.
 
-	- Using this data, we can plot the daily returns for arbitrary coins on a time axis. Example: **Comparing returns for bitcoin, ethereum and ripple**
+  > Example: **Bitcoin**.
 
-	![Returns for bitcoin, ethereum and ripple](https://raw.githubusercontent.com/prouast/cryptocurrency-analysis/master/Coin-returns.png?style=centerme)
+  ![Bitcoin statistics](https://raw.githubusercontent.com/prouast/cryptocurrency-analysis/master/Bitcoin-statistics.png?style=centerme)
 
-	- This also allows us to investigate the correlations between the daily returns of arbitrary coins. Example: **Plotting bitcoin vs ethereum returns**. Here, every point represents one day. *Note that this plot is based on data from 2017 only.*
+4. Comparing different coins directly.
 
-		> Correlation measures the linear relationship between two sets of data. A high value when comparing the daily returns of two coins indicates a positive linear relationship of the returns in the past.
+  - Using the already calculated statistics, we can plot daily market capitalisations, returns and volatilities of arbitrary coins on a time axis.
 
-	![Returns for bitcoin vs ethereum](https://raw.githubusercontent.com/prouast/cryptocurrency-analysis/master/Bitcoin-vs-ethereum-returns.png?style=centerme)
+  > Example: **Bitcoin, Ethereum and Ripple**.
 
-	- Finally, we can look at pairwise correlations between all coins: **Visualisation of the correlation matrix for top 25 cryptocurrencies**. *Note that this plot is based on data from 2017 only.*
+	![Comparing bitcoin, ethereum and ripple](https://raw.githubusercontent.com/prouast/cryptocurrency-analysis/master/Coin-statistics.png?style=centerme)
 
-		> The (symmetric) correlation matrix visualises the correlation for each pair of variables - from perfect positive linear relationship (blue) to perfect negative linear relationship (red).
+	- We can also investigate the correlations between the daily returns of arbitrary coins. Correlation measures the linear relationship between two sets of data. A high positive value when comparing the daily returns of two coins indicates a strong positive linear relationship of the returns in the past.
+
+  > To illustrate the idea of correlation between two coin returns: **Plotting Bitcoin vs Ethereum returns**. Here, every point represents one day. Notice the slightly positive linear relationship? *Note that this plot is based on data from 2017 only.*
+
+	![Returns bitcoin vs ethereum](https://raw.githubusercontent.com/prouast/cryptocurrency-analysis/master/Bitcoin-vs-ethereum-returns.png?style=centerme)
+
+	- To get a better idea of the current relationships between coin returns, we can look at pairwise correlations between all coins. The (symmetric) correlation matrix visualises the correlation for each pair of variables - from perfect positive linear relationship (blue) to perfect negative linear relationship (red).
+
+  > **Visualisation of the correlation matrix for top 25 cryptocurrencies**. *Note that this plot is based on data from 2017 only.*
 
 	![Corrplot](https://raw.githubusercontent.com/prouast/cryptocurrency-analysis/master/Corrplot.png?style=centerme)
 
-4. Calculate overall **market returns** using daily values for overall market capitalisation.
-   This series is initially dominated by bitcoin, with more altcoin influence as their market capitalisations increase.
+5. Comparing individual coins with the overall market.
 
-	- Corresponding to the first plot, we can now give a plot of the overall **Market return** across time:
+	- Similarly to a previous plot, we can investigate the correlation of arbitrary coin returns with the market return.
 
-	![Market return](https://raw.githubusercontent.com/prouast/cryptocurrency-analysis/master/Market-return.png?style=centerme)
+  > Example: **Plotting Ethereum vs Market return**. *Note that this plot is based on data from 2017 only.*
 
-	- Similarly, we can investigate the correlation of arbitrary coins with the market return. Example: **Plotting ethereum vs market return**. *Note that this plot is based on data from 2017 only.*
+  ![Returns eitcoin vs market](https://raw.githubusercontent.com/prouast/cryptocurrency-analysis/master/Ethereum-vs-market-returns.png?style=centerme)
 
-	![Market return](https://raw.githubusercontent.com/prouast/cryptocurrency-analysis/master/Ethereum-vs-market-return.png?style=centerme)
+  - Calculate coin `beta` to characterise the behaviour of each coin return with respect to the market return. Beta represents the covariance of coin returns and market returns, scaled by the variance of the market returns. See [Wikipedia](https://en.wikipedia.org/wiki/Capital_asset_pricing_model) for more information.
 
-5. Calculate coin `beta` to characterise the behaviour of each coin return with respect to the market return.
-
-	- Beta represents the covariance of coin returns and market returns, scaled by the variance of the market returns.
-	  See [Wikipedia](https://en.wikipedia.org/wiki/Capital_asset_pricing_model) for more information.
-
-		> Investors use the information encoded in the beta coefficient to characterise an asset's volatility and tendency to move in accordance with the market index. `beta = 1` indicates that the asset moves exactly like the market index. [More information.](https://en.wikipedia.org/wiki/Beta_(finance)) Here, the market is dominated by bitcoin, hence bitcoin's beta is very close to 1 and a coin's beta can also be interpreted as a comparison to bitcoin's movement.
+	 > Investors use the information encoded in the beta coefficient to characterise an asset's volatility and tendency to move in accordance with the market index. `beta = 1` indicates that the asset moves exactly like the market index. [More information.](https://en.wikipedia.org/wiki/Beta_(finance)) Here, the market is dominated by bitcoin, hence bitcoin's beta is very close to 1 and a coin's beta can also be interpreted as a comparison to bitcoin's movement.
 
 	- Here, we plot coin betas against market capitalisations for the top 20 coins in terms of market capitalisation. *Note that this plot is based on data from 2017 only.*
 
--- analysis.R
+++ analysis.R
@@ -1,4 +1,4 @@
-rm(list=ls(all=TRUE))
+rm(list=ls(all=TRUE)) # Remove everything from environment
 
 # install.packages("RSQLite")
 # install.packages("ggplot2")
@@ -11,13 +11,17 @@ library(corrplot)
 library(zoo)
 library(magrittr)
 
-### Import data from sqlite and prepare
-con <- dbConnect(RSQLite::SQLite(), dbname='database.db')
-coins <- dbGetQuery(con, "SELECT * FROM coin")
-vals <- dbGetQuery(con, "SELECT * FROM vals")
+### 1. Import and clean daily closing prices for each coin
+
+# Import from sqlite
+con <- dbConnect(RSQLite::SQLite(), dbname='database.db') # Database connection
+coins <- dbGetQuery(con, "SELECT * FROM coin") # Import coins
+vals <- dbGetQuery(con, "SELECT * FROM vals") # Import values
+rm(con) # Close database connection
+
+# Clean and prepare data
 vals$id <- NULL # Drop database IDs
 coins$id <- NULL # Drop database IDs
-rm(con) # Close database connection
 vals$datetime <- as.Date(vals$datetime) # Format dates
 vals <- vals[!duplicated(vals[,6:7]),] # Remove duplicates/one price per day
 interpolate.missing.data <- function(data) {
@@ -43,101 +47,187 @@ missing.date.rows <- function(coin, data) {
                     datetime=as.Date(missingdates, origin="1970-01-01"),
                     coin_slug=rep(coin, length(missingdates))))
 }
-vals <- interpolate.missing.data(vals) # Insert missing dates and interpolate values
+vals <- interpolate.missing.data(vals) # For missing dates, insert fields and interpolate values (takes some time)
 
-### Analysis
+### 2. Calculate overall market statistics
 
-# Calculate returns
-vals$return <- Reduce(c,sapply(unique(vals$coin_slug), FUN=function(x) c(0,diff(vals[vals$coin_slug==x,]$price_usd)/(vals[vals$coin_slug==x,]$price_usd)[-length(vals[vals$coin_slug==x,]$price_usd)])))
-vals$logreturn <- Reduce(c,sapply(unique(vals$coin_slug), FUN=function(x) c(0,log(vals[vals$coin_slug==x,]$price_usd[-1]/vals[vals$coin_slug==x,]$price_usd[-length(vals[vals$coin_slug==x,]$price_usd)]))))
-
-# Calculate market data
-market.data <- function(vals) {
-  dates <- sort(unique(vals$datetime))
-  cap <- sapply(dates, FUN=function(date) sum(vals[vals$datetime==date,4]))
+## Calculate market statistics
+# returns: return(t) = (price(t) - price(t-1)) / price(t-1)
+# logreturns: logreturn(t) = ln(price(t)/price(t-1))
+# annualized volatility: sd(logreturns per x days)*sqrt(trading days=365)
+# herfindahl: sum of squares of competitor market shares
+market.data <- function(data) {
+  dates <- sort(unique(data$datetime))
+  cap <- sapply(dates, FUN=function(date) sum(data[data$datetime==date,4]))
   returns <- c(0,diff(cap)/cap[-length(cap)])
   logreturns <- c(0,log(cap[-1]/cap[-length(cap)]))
-  data.frame(datetime=dates, cap=cap, returns=returns, logreturns=logreturns)
+  volatility.30d <- sapply(1:length(logreturns), FUN=function(i) sd(logreturns[(max(i-30,0):i)]))*sqrt(365)
+  volatility.90d <- sapply(1:length(logreturns), FUN=function(i) sd(logreturns[(max(i-90,0):i)]))*sqrt(365)
+  herfindahl <- sapply(dates, FUN=function(date) sum((data[vals$datetime==date,4]/sum(data[data$datetime==date,4]))^2))
+  data.frame(datetime=dates, cap=cap, return=returns, logreturn=logreturns, volatility.30d=volatility.30d, volatility.90d=volatility.90d, herfindahl=herfindahl)
 }
 market <- market.data(vals)
 
-# Calculate Herfindahl index for each day
-market$herfindahl <- sapply(market$datetime, FUN=function(date) sum((vals[vals$datetime==date,4]/sum(vals[vals$datetime==date,4]))^2))
-
-# Calculate betas
-coin.beta <- function(coin, data, market) {
-  dates <- intersect(data[data$coin_slug==coin,]$datetime, market$datetime)
-  return(cov(data[data$coin_slug==coin & data$datetime %in% dates,]$return,
-             market[market$datetime %in% dates,]$returns)/var(market[market$datetime %in% dates,]$returns))
+# Plot market cap, market return, market volatility and herfindahl index
+plot.market <- function(market) {
+  p1 <- ggplot(market, aes(datetime, cap)) +
+    geom_line() +
+    labs(x="Date", y="Market cap", title="Overall market") +
+    theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
+  p2 <- ggplot(market, aes(datetime, logreturn)) +
+    geom_line() +
+    labs(x="Date", y="Log return") +
+    theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
+  p3 <- ggplot(market, aes(datetime, volatility.30d)) +
+    geom_line() +
+    labs(x="Date", y="Annualized volatility") +
+    theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
+  p4 <- ggplot(market, aes(datetime, herfindahl)) + geom_line() + labs(x="Date", y="Herfindahl index")
+  ## convert plots to gtable objects
+  library(gtable)
+  library(grid) # low-level grid functions are required
+  g1 <- ggplotGrob(p1)
+  g2 <- ggplotGrob(p2)
+  g3 <- ggplotGrob(p3)
+  g4 <- ggplotGrob(p4)
+  g <- rbind(g1, g2, g3, g4, size="first") # stack the plots
+  g$widths <- unit.pmax(g1$widths, g2$widths, g3$widths, g4$widths) # use the largest widths
+  # center the legend vertically
+  g$layout[grepl("guide", g$layout$name),c("t","b")] <- c(1,nrow(g))
+  grid.newpage()
+  grid.draw(g)
 }
-coins$beta <- sapply(coins$slug, FUN=coin.beta, vals[vals$datetime>as.Date("2016-12-31"),], market)
+plot.market(market)
+
+### 3. Calculate individual coin statistics
 
 # Fetch latest market capitalisation per coin
 coins$mcap <- sapply(coins$slug, FUN=function(x) vals[vals$coin_slug==x & vals$datetime==max(vals[vals$coin_slug==x,]$datetime),]$market_cap_usd)
 coins <- coins[order(coins$mcap,coins$slug, decreasing=TRUE),]; rownames(coins) <- 1:nrow(coins) # Sort
 
-### Plots
+# Calculate returns
+vals$return <- Reduce(c,sapply(unique(vals$coin_slug), FUN=function(x) c(0,diff(vals[vals$coin_slug==x,]$price_usd)/(vals[vals$coin_slug==x,]$price_usd)[-length(vals[vals$coin_slug==x,]$price_usd)])))
+vals$logreturn <- Reduce(c,sapply(unique(vals$coin_slug), FUN=function(x) c(0,log(vals[vals$coin_slug==x,]$price_usd[-1]/vals[vals$coin_slug==x,]$price_usd[-length(vals[vals$coin_slug==x,]$price_usd)]))))
+
+# Calculate volatility (takes too long - do on demand in plot function)
+#vals$volatility.30d <- Reduce(c,sapply(unique(vals$coin_slug), FUN=function(x) sapply(1:length(vals[vals$coin_slug==x,]$logreturn), FUN=function(i) sd(vals[vals$coin_slug==x,]$logreturn[(max(i-30,0):i)]))))
+#vals$volatility.90d <- Reduce(c,sapply(unique(vals$coin_slug), FUN=function(x) sapply(1:length(vals[vals$coin_slug==x,]$logreturn), FUN=function(i) sd(vals[vals$coin_slug==x,]$logreturn[(max(i-90,0):i)]))))
+
+# Plot coin cap, return and volatility
+plot.coin <- function(data, slug) {
+  data <- data[data$coin_slug==slug,]
+  data$volatility.30d <- sapply(1:nrow(data), FUN=function(i) sd(data$logreturn[(max(i-30,0):i)]))*sqrt(365)
+  p1 <- ggplot(data, aes(datetime, market_cap_usd)) +
+    geom_line() +
+    labs(x="Date", y="Market cap", title=slug) +
+    theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
+  p2 <- ggplot(data, aes(datetime, logreturn)) +
+    geom_line() + labs(x="Date", y="Log return") +
+    theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
+  p3 <- ggplot(data, aes(datetime, volatility.30d)) + geom_line() + labs(x="Date", y="Annualized volatility")
+  ## convert plots to gtable objects
+  library(gtable)
+  library(grid) # low-level grid functions are required
+  g1 <- ggplotGrob(p1)
+  g2 <- ggplotGrob(p2)
+  g3 <- ggplotGrob(p3)
+  g <- rbind(g1, g2, g3, size="first") # stack the plots
+  g$widths <- unit.pmax(g1$widths, g2$widths, g3$widths) # use the largest widths
+  # center the legend vertically
+  g$layout[grepl("guide", g$layout$name),c("t","b")] <- c(1,nrow(g))
+  grid.newpage()
+  grid.draw(g)
+}
+plot.coin(vals, "bitcoin")
+
+### 4. Comparing different coins directly
+
+# Plot coin cap, return and volatility for multiple coins
+plot.coins <- function(data, slugs) {
+  data <- data[data$coin_slug %in% slugs,]
+  data$volatility.30d <- Reduce(c,sapply(unique(data$coin_slug), FUN=function(x) sapply(1:length(data[data$coin_slug==x,]$logreturn), FUN=function(i) sd(data[data$coin_slug==x,]$logreturn[(max(i-30,0):i)]))))*sqrt(365)
+  p1 <- ggplot(data, aes(datetime, market_cap_usd, color=factor(coin_slug))) +
+    geom_line() +
+    labs(x="Date", y="Market cap", title=paste(slugs, collapse=", ")) +
+    theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank(), legend.title=element_blank())
+  p2 <- ggplot(data, aes(datetime, logreturn, color=factor(coin_slug))) +
+    geom_line() +
+    labs(x="Date", y="Log return") +
+    theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank(), legend.title=element_blank())
+  p3 <- ggplot(data, aes(datetime, volatility.30d, color=factor(coin_slug))) +
+    geom_line() +
+    labs(x="Date", y="Annualized volatility")
+  ## convert plots to gtable objects
+  library(gtable)
+  library(grid) # low-level grid functions are required
+  g1 <- ggplotGrob(p1)
+  g2 <- ggplotGrob(p2)
+  g3 <- ggplotGrob(p3)
+  g <- rbind(g1, g2, g3, size="first") # stack the plots
+  g$widths <- unit.pmax(g1$widths, g2$widths, g3$widths) # use the largest widths
+  # center the legend vertically
+  g$layout[grepl("guide", g$layout$name),c("t","b")] <- c(1,nrow(g))
+  grid.newpage()
+  grid.draw(g)
+}
+plot.coins(vals, c("bitcoin","ethereum", "ripple"))
 
 # Generates a dataframe with complete daily information for a set of coins
 analysis.data <- function(coins, data, market=NULL) {
   temp <- lapply(coins, FUN=function(x) subset(data, coin_slug==x))
   temp <- Reduce(function(df1, df2) merge(df1, df2, by="datetime"), temp)
   if (length(coins) > 1)
-    colnames(temp) <- c("datetime", sapply(coins, function(slug) sapply(colnames(vals)[c(1:5,7:9)], function(x) paste(x, slug, sep="_"))))
+    colnames(temp) <- c("datetime", sapply(coins, function(slug) sapply(colnames(data)[c(1:5,7:9)], function(x) paste(x, slug, sep="_"))))
   if (!is.null(market))
     temp <- merge(temp, market, by="datetime")
   data.frame(temp)
 }
 
-# Generates a dataframe with daily returns for a set of coins
-analysis.return.data <- function(coins, data) {
-  data <- reshape(data[data$coin_slug %in% coins,c(6:8)], direction="wide", idvar="datetime", timevar="coin_slug")
-  colnames(data) <- c("datetime", sort(coins))
-  data <- data[,c("datetime", coins)]
-  return(data)
-}
-corrplot(cor(analysis.return.data(coins[1:25,]$slug,vals[vals$datetime>as.Date("2016-12-31"),])[,-1],
-             use = "pairwise.complete.obs"), method="ellipse")
-
-# Plot return timelines
-plot.return.timeline <- function(coins, data) {
-  p <- ggplot(data[data$coin_slug %in% coins,], aes(datetime, return, color=factor(coin_slug)))
-  p + geom_line() + 
-    labs(title="Cryptocurrency returns", x="Date", y="Return") +
-    theme(legend.title=element_blank())
-}
-plot.return.timeline(c("bitcoin","ethereum","ripple"), vals)
-
-# Plot market return timeline
-plot.market.return.timeline <- function(market) {
-  p <- ggplot(market, aes(datetime, returns))
-  p + geom_line() + 
-    labs(title="Cryptocurrency market return", x="Date", y="Return") +
-    theme(legend.title=element_blank())
-}
-plot.market.return.timeline(market)
-
 # Plot returns against each other
 plot.return.vs.return <- function(coin1, coin2, data) {
   data <- analysis.data(c(coin1, coin2), data)
-  cor_ <- cor(data[[paste("return_",coin1,sep="")]], data[[paste("return_",coin2,sep="")]])
-  p <- ggplot(data, aes_string(x=paste("return_",coin1,sep=""), y=paste("return_",coin2,sep="")))
+  cor_ <- cor(data[[paste("logreturn_",coin1,sep="")]], data[[paste("logreturn_",coin2,sep="")]])
+  p <- ggplot(data, aes_string(x=paste("logreturn_",coin1,sep=""), y=paste("logreturn_",coin2,sep="")))
   p + geom_point() +
     labs(title=paste("Returns: ",coin1," vs ",coin2," (cor = ",round(cor_, digits=4),")",sep=""), x=paste(coin1, "Return"), y=paste(coin2, "Return")) +
     theme(legend.title=element_blank())
 }
 plot.return.vs.return("bitcoin", "ethereum", vals[vals$datetime>as.Date("2016-12-31"),])
 
+# Generates a dataframe with daily returns for a set of coins
+analysis.return.data <- function(coins, data) {
+  data <- reshape(data[data$coin_slug %in% coins,c(6,7,9)], direction="wide", idvar="datetime", timevar="coin_slug")
+  colnames(data) <- c("datetime", sort(coins))
+  data <- data[,c("datetime", coins)]
+  return(data)
+}
+
+# Plot the correlation matrix for top 25 coin returns
+corrplot(cor(analysis.return.data(coins[1:25,]$slug,vals[vals$datetime>as.Date("2016-12-31"),])[,-1],
+             use = "pairwise.complete.obs"), method="ellipse")
+
+# TODO Correlation between coins over time
+
+### 5. Comparing coins with overall market
+
 # Plot return against weighted market return
 plot.return.vs.market <- function(coin, data, market) {
   data <- analysis.data(coin, data, market)
-  cor_ <- cor(data$return, data$returns)
-  p <- ggplot(data, aes(x=return, y=returns))
+  cor_ <- cor(data$logreturn.x, data$logreturn.y)
+  p <- ggplot(data, aes(x=logreturn.x, y=logreturn.y))
   p + geom_point() +
     labs(title=paste("Returns: ",coin," vs Market (cor = ",round(cor_, digits=4),")",sep=""), x=paste(coin, "return"), y="Market return") +
     theme(legend.title=element_blank())
 }
 plot.return.vs.market("ethereum", vals[vals$datetime>as.Date("2016-12-31"),], market)
+
+# Calculate betas
+coin.beta <- function(coin, data, market) {
+  dates <- intersect(data[data$coin_slug==coin,]$datetime, market$datetime)
+  return(cov(data[data$coin_slug==coin & data$datetime %in% dates,]$logreturn,
+             market[market$datetime %in% dates,]$logreturn)/var(market[market$datetime %in% dates,]$logreturn))
+}
+coins$beta <- sapply(coins$slug, FUN=coin.beta, vals[vals$datetime>as.Date("2016-12-31"),], market)
 
 # Plot betas of top currencies against latest market cap
 plot.beta.vs.mcap.num <- function(num, coins) {
@@ -153,22 +243,4 @@ plot.beta.vs.mcap.num <- function(num, coins) {
 }
 plot.beta.vs.mcap.num(25, coins)
 
-# Plot total market cap and herfindahl index
-plot.mcap.herfindahl <- function(market) {
-  p1 <- ggplot(market, aes(datetime, cap)) + geom_line() + labs(x="Date", y="Market cap")
-  p2 <- ggplot(market, aes(datetime, herfindahl)) + geom_line() + labs(x="Date", y="Herfindahl index")
-  ## convert plots to gtable objects
-  library(gtable)
-  library(grid) # low-level grid functions are required
-  g1 <- ggplotGrob(p1)
-  #g1 <- gtable_add_cols(g1, unit(0,"mm")) # add a column for missing legend
-  g2 <- ggplotGrob(p2)
-  g <- rbind(g1, g2, size="first") # stack the two plots
-  g$widths <- unit.pmax(g1$widths, g2$widths) # use the largest widths
-  # center the legend vertically
-  g$layout[grepl("guide", g$layout$name),c("t","b")] <- c(1,nrow(g))
-  grid.newpage()
-  grid.draw(g)
-}
-plot.mcap.herfindahl(market)
-
+# TODO Beta over time
